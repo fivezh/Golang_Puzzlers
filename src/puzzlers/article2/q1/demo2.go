519389952864e5884249a239271618bf1e3f1661
Fivezh <fivezh@gmail.com>
2018-08-15 07:23:50 +0100

    add fivezh note for article2

--- src/puzzlers/article2/q1/demo2.go
+++ src/puzzlers/article2/q1/demo2.go
@@ -5,13 +5,16 @@ import (
 	"fmt"
 )
 
-var name string
-
-func init() {
-	flag.StringVar(&name, "name", "everyone", "The greeting object.")
-}
-
 func main() {
+	// 方式1： 读取参数直接赋值给变量
+	var foo string
+	flag.StringVar(&foo, "foo", "everyone", "The greeting object.")
+
+	// 方式2：读取参数返回指针
+	var bar *string
+	bar = flag.String("bar", "everyone", "The greeting object.")
+
 	flag.Parse()
-	fmt.Printf("Hello, %s!\n", name)
+	fmt.Printf("Hello, %s!\n", foo)
+	fmt.Printf("Hello, %s!\n", *bar)
 }
